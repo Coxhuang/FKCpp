7044e7210235652291a76f668ea8cbac5655717a
huangminhang <mhesat@outlook.com>
2020-09-10 11:13:05 +0800

    update

---
+++ 3.class/cmake-build-debug/CMakeFiles/3_class.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "3_class"
+  "3_class.pdb"
+  "CMakeFiles/3_class.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_class.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3.class/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Point{
+
+    public:
+        void setPoint(int x, int y){
+            xPos = x;
+            yPos = y;
+        }
+
+        void printPoint() const{
+            cout<< "x = " << xPos << endl;
+            cout<< "y = " << yPos << endl;
+        }
+
+    private:
+        int xPos;
+        int yPos;
+};
+
+int main()
+{
+    Point M{};        //用定义好的类创建一个对象 点M
+    M.setPoint(10, 20); //设置 M点 的x,y值
+    M.printPoint();     //输出 M点 的信息
+
+    return 0;
+}
