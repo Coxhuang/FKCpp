49ff7df35b2261be7855320dc478de4c8f1049f3
huangminhang <mhesat@outlook.com>
2020-10-01 14:25:57 +0800

    add boost demo

---
+++ 6.boost_demo/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <boost/version.hpp>
+
+int main() {
+    std::cout << "Hello, World" << std::endl;
+    std::cout << "Boost版本：" << BOOST_VERSION << std::endl;
+    return 0;
+}
