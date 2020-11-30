a0b02d48d6f9bf445a32ade87ef030f2241bd9ce
huangminhang <mhesat@outlook.com>
2020-11-30 18:19:54 +0800

    update grpc

--- gRPCDemo/client_cpp/main.cpp
+++ gRPCDemo/client_cpp/main.cpp
@@ -6,4 +6,63 @@
  *
  * Written by Huang Minhang <huangminhang@trunk.tech>, 2020/11/30 6:02 下午
  */
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/helloworld.grpc.pb.h"
+#else
+#include "../protos/helloworld.grpc.pb.h"
+#endif
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::TestServer; // Greeter
+using helloworld::HelloMessage; // HelloRequest
+using helloworld::Reply; // HelloReply
+
+
+class GreeterClient {
+public:
+
+    GreeterClient(std::shared_ptr<Channel> channel):stub_(TestServer::NewStub(channel)) {}
+
+    int say_hello(const std::string& user) {
+
+        HelloMessage request;
+        Reply reply;
+        ClientContext context;
+
+        request.set_a(21);
+        request.set_b(22);
+
+        Status status = stub_->hello_request(&context, request, &reply);
+
+        if (status.ok()) {
+            return reply.result();
+        } else {
+            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+            return 0;
+        }
+    }
+
+private:
+    std::unique_ptr<TestServer::Stub> stub_;
+
+};
+
+int main(int argc, char** argv) {
+
+    GreeterClient greeter(grpc::CreateChannel("127.0.0.1:5000", grpc::InsecureChannelCredentials()));
+    std::string user("world");
+    int reply = greeter.say_hello(user);
+    std::cout << "Greeter received: " << reply << std::endl;
+
+    return 0;
+}
  
--- gRPCDemo/main.cpp
+++ gRPCDemo/main.cpp
@@ -1,47 +1,11 @@
 
-//protoc --cpp_out=. helloworld.proto
-//protoc --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` helloworld.proto
-
+// protoc --cpp_out=. helloworld.proto
+// protoc --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` helloworld.proto
+// python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. helloworld.proto
 
 #include <string>
-#include <grpcpp/grpcpp.h>
-#include "protos/helloworld.grpc.pb.h"
-
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
-
-using helloworld::TestServer; // MathTest
-using helloworld::HelloMessage; // MathRequest
-using helloworld::Reply; // MathReply
-
-class HelloServiceImplementation final : public TestServer::Service {
-    Status hello_request(
-            ServerContext* context,
-            const HelloMessage* request,
-            Reply* reply
-    ) override {
-        int a = request->a();
-        int b = request->b();
-        reply->set_result(a * b);
-        return Status::OK;
-    }
-};
-
-void Run() {
-    std::string address("0.0.0.0:5000");
-    HelloServiceImplementation service;
-    ServerBuilder builder;
-    builder.AddListeningPort(address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-    std::unique_ptr<Server> server(builder.BuildAndStart());
-    std::cout << "Server listening on port: " << address << std::endl;
-    server->Wait();
-}
 
 int main(int argc, char** argv) {
-    Run();
     return 0;
 }
 
