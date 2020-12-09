f01299e5863314399e8686f9b6d6af97da25e233
huangminhang <mhesat@outlook.com>
2020-12-09 22:54:35 +0800

    update

---
+++ libwebsocketdemo/cmake-build-debug/CMakeFiles/libwebsocketdemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libwebsocketdemo.dir/main.cpp.o"
+  "libwebsocketdemo"
+  "libwebsocketdemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libwebsocketdemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ my_extend/src/b/b.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) Trunk Technology, Inc. - All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by Huang Minhang <huangminhang@trunk.tech>, 2020/12/4 4:50 下午
+ */
+ 
+#ifndef MY_EXTEND_B_H
+#define MY_EXTEND_B_H
+
+#endif //MY_EXTEND_B_H
