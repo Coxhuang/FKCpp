03c066b979a42abeacffcacf6a86e7e885ed7cbf
huangminhang <mhesat@outlook.com>
2021-02-06 16:44:56 +0800

    update yaml demo

--- 15.yaml_demo/main.cpp
+++ 15.yaml_demo/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "yaml-cpp/yaml.h"
 
 int main() {
-    std::cout << "Hello, Yaml!" << std::endl;
+    YAML::Node config = YAML::LoadFile("../config.yaml");
+    std::cout << "name:" << config["name"].as<std::string>() << std::endl;
+    std::cout << "sex:" << config["sex"].as<std::string>() << std::endl;
+    std::cout << "age:" << config["age"].as<int>() << std::endl;
+
+    for (auto foo : config["skills"]) {
+        std::cout << foo.first.as<std::string>() << ":" << foo.second.as<bool>() << std::endl;
+    }
+
     return 0;
 }
